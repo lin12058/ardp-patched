b4b001a0dc7dd566f1b27993e8447ec187f8c6ff
6g3y <37627813+lin12058@users.noreply.github.com>
2026-06-18 09:10:18 +0800

    Backport lyc8503 accessibility service logic

--- bVNC/src/main/java/com/iiordanov/util/KeyboardAccessibilityService.java
+++ bVNC/src/main/java/com/iiordanov/util/KeyboardAccessibilityService.java
@@ -1,43 +1,70 @@
 package com.iiordanov.util;
 
 import android.accessibilityservice.AccessibilityService;
+
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
+
 import com.iiordanov.bVNC.RemoteCanvasActivity;
 import com.iiordanov.bVNC.input.RemoteRdpKeyboard;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class KeyboardAccessibilityService extends AccessibilityService {
 
-    private final static List<Integer> BAD = Arrays.asList(
-        KeyEvent.KEYCODE_VOLUME_UP,
-        KeyEvent.KEYCODE_VOLUME_DOWN,
-        KeyEvent.KEYCODE_POWER
-    );
+    private static final String TAG = "aRDPAccessibility";
+    private final static List<Integer> BLACKLISTED_KEYS = Arrays.asList(KeyEvent.KEYCODE_VOLUME_UP, KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_POWER);
 
-    public boolean onKeyEvent(KeyEvent e) {
-        if (!RemoteCanvasActivity.hasForegroundInstance) return super.onKeyEvent(e);
-        if (BAD.contains(e.getKeyCode())) return super.onKeyEvent(e);
-        if (RemoteRdpKeyboard.instance != null && RemoteRdpKeyboard.instance.keyboardActive) {
-            if (e.getAction() == KeyEvent.ACTION_DOWN)
-                RemoteRdpKeyboard.instance.sendKeyEvent(e, true);
-            else
-                RemoteRdpKeyboard.instance.sendKeyEvent(e, false);
-            return true;
+    @Override
+    public boolean onKeyEvent(KeyEvent event) {
+
+        if (!RemoteCanvasActivity.hasForegroundInstance) {
+            // If the app is not in the foreground, we don't want to process the key event
+            return super.onKeyEvent(event);
         }
-        return super.onKeyEvent(e);
+
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        boolean isMod = KeyEvent.isModifierKey(keyCode);
+
+        Log.d(TAG, "onKeyEvent: code " + keyCode + ", action " + action + ", modifier " + isMod);
+
+        if (RemoteRdpKeyboard.instance != null && RemoteRdpKeyboard.instance.isConnected() && !BLACKLISTED_KEYS.contains(keyCode)) {
+            if (action == KeyEvent.ACTION_DOWN) {
+                if (!isMod) {
+                    RemoteRdpKeyboard.instance.repeatKeyEvent(keyCode, event);
+                } else {
+                    RemoteRdpKeyboard.instance.processLocalKeyEvent(keyCode, event, 0);
+                }
+                return true;
+            } else if (action == KeyEvent.ACTION_UP) {
+                if (!isMod) {
+                    RemoteRdpKeyboard.instance.stopRepeatingKeyEvent();
+                } else {
+                    RemoteRdpKeyboard.instance.processLocalKeyEvent(keyCode, event, 0);
+                }
+                return true;
+            }
+        }
+
+        Log.d(TAG, "onKeyEvent: no active connection, key event not processed");
+        return super.onKeyEvent(event);
     }
 
+    @Override
     public void onServiceConnected() {
-        super.onServiceConnected();
-        Log.d("ACC", "connected");
+        Log.i(TAG, "onServiceConnected: Keyboard service is connected");
     }
 
-    public void onAccessibilityEvent(AccessibilityEvent e) {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
     }
 
+    @Override
     public void onInterrupt() {
+
     }
 }
